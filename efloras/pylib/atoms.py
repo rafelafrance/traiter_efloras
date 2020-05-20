a22ad834ed85922d42667c2d69f4c3f96042b95d
rafe <raphael.lafrance@gmail.com>
2020-05-20 15:47:44 -0400

    Atom now use sets of traits instead of list

--- efloras/pylib/atoms.py
+++ efloras/pylib/atoms.py
@@ -2,102 +2,100 @@
 
 import regex
 
-from traiter.util import FLAGS
+from traiter.util import FLAGS  # pylint: disable=import-error
 
 
-ALL = ['seasonal_descriptor', 'sexual_descriptor', 'symmetry_descriptor',
-       'temporal_descriptor']
-CALYX = ['calyx_size', 'caylx_shape', 'caylx_color']
-COROLLA = ['corolla_size', 'corolla_shape', 'corolla_color']
-FLOWER = ['flower_shape', 'flower_size', 'flower_color']
-FRUIT = ['fruit_shape', 'fruit_size', 'fruit_color']
-HYPANTHIUM = ['hypanthium_shape', 'hypanthium_size', 'hypanthium_color']
-LEAF = ['leaf_size', 'leaf_shape']
-PETAL = ['petal_size', 'petal_shape', 'petal_color']
-PETIOLE = ['petiole_size', 'petiole_shape']
-SEED = ['seed_size']
-SEPAL = ['sepal_size', 'sepal_shape', 'sepal_color']
+ALL = {'seasonal_descriptor', 'sexual_descriptor', 'symmetry_descriptor',
+       'temporal_descriptor'}
+CALYX = {'calyx_size', 'caylx_shape', 'caylx_color'}
+COROLLA = {'corolla_size', 'corolla_shape', 'corolla_color'}
+FLOWER = {'flower_shape', 'flower_size', 'flower_color'}
+FRUIT = {'fruit_shape', 'fruit_size', 'fruit_color'}
+HYPANTHIUM = {'hypanthium_shape', 'hypanthium_size', 'hypanthium_color'}
+LEAF = {'leaf_size', 'leaf_shape'}
+PETAL = {'petal_size', 'petal_shape', 'petal_color'}
+PETIOLE = {'petiole_size', 'petiole_shape'}
+SEED = {'seed_size'}
+SEPAL = {'sepal_size', 'sepal_shape', 'sepal_color'}
 
 
 # Keywords used to split treatment into text atoms
 ATOMS = {
-    r'2n': [],
-    'anther heads': [],
-    'anthers': [],
-    'asexual structures': [],
-    'basal leaves': (LEAF + PETIOLE),
-    'basal rosettes': [],
-    'calyptra': [],
+    r'2n': set(),
+    'anther heads': set(),
+    'anthers': set(),
+    'asexual structures': set(),
+    'basal leaves': (LEAF | PETIOLE),
+    'basal rosettes': set(),
+    'calyptra': set(),
     'capsule': FRUIT,
-    'capsules': [],
-    'caudices': [],
-    'cauline leaves': (LEAF + PETIOLE),
+    'capsules': set(),
+    'caudices': set(),
+    'cauline leaves': (LEAF | PETIOLE),
     'corollas': (
-        FLOWER + HYPANTHIUM + SEPAL + PETAL + CALYX + COROLLA),
+        FLOWER | HYPANTHIUM | SEPAL | PETAL | CALYX | COROLLA),
     'flowering stems': (
-        FLOWER + HYPANTHIUM + SEPAL + PETAL + CALYX + COROLLA),
+        FLOWER | HYPANTHIUM | SEPAL | PETAL | CALYX | COROLLA),
     'flowers': (
-        FLOWER + HYPANTHIUM + SEPAL + PETAL + CALYX + COROLLA),
-    'fruiting peduncles': [],
+        FLOWER | HYPANTHIUM | SEPAL | PETAL | CALYX | COROLLA),
+    'fruiting peduncles': set(),
     'fruits': FRUIT,
-    'herbs': [],
+    'herbs': set(),
     'hypanthia': (
-        FLOWER + HYPANTHIUM + SEPAL + PETAL + CALYX + COROLLA),
-    'inflorescences': [],
-    'inflorescenses': [],
-    'leaf blades': (LEAF + PETIOLE),
-    'leaflets': (LEAF + PETIOLE),
-    'leaves': (LEAF + PETIOLE),
-    'pedicels': [],
-    'peduncles': [],
+        FLOWER | HYPANTHIUM | SEPAL | PETAL | CALYX | COROLLA),
+    'inflorescences': (
+        FLOWER | HYPANTHIUM | SEPAL | PETAL | CALYX | COROLLA),
+    'inflorescenses': (
+        FLOWER | HYPANTHIUM | SEPAL | PETAL | CALYX | COROLLA),
+    'leaf blades': (LEAF | PETIOLE),
+    'leaflets': (LEAF | PETIOLE),
+    'leaves': (LEAF | PETIOLE),
+    'pedicels': set(),
+    'peduncles': set(),
     'pepos': FRUIT,
     'petals': (
-        FLOWER + HYPANTHIUM + SEPAL + PETAL + CALYX + COROLLA),
-    'petioles': (LEAF + PETIOLE),
+        FLOWER | HYPANTHIUM | SEPAL | PETAL | CALYX | COROLLA),
+    'petioles': (LEAF | PETIOLE),
     'pistillate corollas': (
-        FLOWER + HYPANTHIUM + SEPAL + PETAL + CALYX + COROLLA),
+        FLOWER | HYPANTHIUM | SEPAL | PETAL | CALYX | COROLLA),
     'pistillate flowers': (
-        FLOWER + HYPANTHIUM + SEPAL + PETAL + CALYX + COROLLA),
+        FLOWER | HYPANTHIUM | SEPAL | PETAL | CALYX | COROLLA),
     'pistillate inflorescences': (
-        FLOWER + HYPANTHIUM + SEPAL + PETAL + CALYX + COROLLA),
+        FLOWER | HYPANTHIUM | SEPAL | PETAL | CALYX | COROLLA),
     'pistillate racemes': (
-        FLOWER + HYPANTHIUM + SEPAL + PETAL + CALYX + COROLLA),
-    'plants': [],
-    'pollen': [],
-    'protonematal flaps': [],
+        FLOWER | HYPANTHIUM | SEPAL | PETAL | CALYX | COROLLA),
+    'plants': set(),
+    'pollen': set(),
+    'protonematal flaps': set(),
     'racemes': (
-        FLOWER + HYPANTHIUM + SEPAL + PETAL + CALYX + COROLLA),
+        FLOWER | HYPANTHIUM | SEPAL | PETAL | CALYX | COROLLA),
     'seeds': SEED,
-    'seta': [],
-    'seta superficial cells': [],
-    'sexual condition': [],
-    'specialized asexual structures': [],
-    'spores': [],
+    'seta': set(),
+    'seta superficial cells': set(),
+    'sexual condition': set(),
+    'specialized asexual structures': set(),
+    'spores': set(),
     'staminate corollas': (
-        FLOWER + HYPANTHIUM + SEPAL + PETAL + CALYX + COROLLA),
+        FLOWER | HYPANTHIUM | SEPAL | PETAL | CALYX | COROLLA),
     'staminate flowers': (
-        FLOWER + HYPANTHIUM + SEPAL + PETAL + CALYX + COROLLA),
+        FLOWER | HYPANTHIUM | SEPAL | PETAL | CALYX | COROLLA),
     'staminate inflorescences': (
-        FLOWER + HYPANTHIUM + SEPAL + PETAL + CALYX + COROLLA),
+        FLOWER | HYPANTHIUM | SEPAL | PETAL | CALYX | COROLLA),
     'staminate racemes': (
-        FLOWER + HYPANTHIUM + SEPAL + PETAL + CALYX + COROLLA),
-    'stem leaves': [],
-    'stems': [],
-    'stolon leaves': [],
-    'stolons': [],
-    'tendrils': [],
-    'thallose protonematal flaps': [],
-    'vines': [],
-    'x': [],
+        FLOWER | HYPANTHIUM | SEPAL | PETAL | CALYX | COROLLA),
+    'stem leaves': set(),
+    'stems': set(),
+    'stolon leaves': set(),
+    'stolons': set(),
+    'tendrils': set(),
+    'thallose protonematal flaps': set(),
+    'vines': set(),
+    'x': set(),
 }
 
-ATOMS = dict(sorted(ATOMS.items(), reverse=True, key=lambda x: len(x[0])))
+ATOMS = dict(sorted(ATOMS.items(), reverse=True, key=lambda a: len(a[0])))
 
 ATOMIZER = ' | '.join(
-    r' \s+ '.join(x.split()) for x in ATOMS.keys())
+    r' \s '.join(x.split()) for x in ATOMS.keys())
 ATOMIZER = regex.compile(
-    rf"""
-        (?<! [\w:;,<>()] \s ) (?<! [\w:;,<>()] )
-        \b ( {ATOMIZER} ) \b
-        """,
-    flags=FLAGS)
+    rf""" (?<! [\w:,<>()] \s) \b ( {ATOMIZER} ) \b  """, flags=FLAGS)
