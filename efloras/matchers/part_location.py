e02201f00afdc68dcb81d5982ef8f00831f1914e
rafe <raphael.lafrance@gmail.com>
2020-07-09 11:07:16 -0400

    Add data to part_location trait

--- efloras/matchers/part_location.py
+++ efloras/matchers/part_location.py
@@ -3,10 +3,10 @@
 from ..pylib.util import TRAIT_STEP
 
 
-def part_location(_):
+def part_location(span):
     """Handle a part that is being used as a location."""
-    data = {'_relabel': 'part_location'}
-    for token in _:
+    data = {'_relabel': 'part_location', 'location': span.lower_}
+    for token in span:
         token._.aux['skip'] = True
     return data
 
