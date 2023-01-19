9b6f2f7a9ed59bcf52a8008c208beff85c9db384
rafelafrance <raphael.lafrance@gmail.com>
2023-01-19 04:53:50 -0500

    Remove redundant tests

--- tests/setup.py
+++ tests/setup.py
@@ -12,9 +12,8 @@ def test(text: str) -> list[dict]:
     doc = NLP(text)
     traits = [e._.data for e in doc.ents]
 
-    from pprint import pp
-
-    pp(traits, compact=True)
+    # from pprint import pp
+    # pp(traits, compact=True)
 
     # from spacy import displacy
     # displacy.serve(doc, options={'collapse_punct': False, 'compact': True})
