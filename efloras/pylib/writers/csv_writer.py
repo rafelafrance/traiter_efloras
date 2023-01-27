02d99594cf40c344cd82d33ff7c386b19ff06121
rafelafrance <raphael.lafrance@gmail.com>
2023-01-27 11:39:29 -0500

    Update writers

--- efloras/pylib/writers/csv_writer.py
+++ efloras/pylib/writers/csv_writer.py
@@ -2,16 +2,7 @@ from plants.writers import csv_writer as base_writer
 
 
 class CsvWriter(base_writer.CsvWriter):
-    @staticmethod
-    def sort_df(df):
-        first = """ family flora_id flora_name taxon taxon_id link path """.split()
-
-        rest = sorted(c for c in df.columns if c not in first)
-        columns = first + rest
-
-        df = df[columns]
-
-        return df
+    first = """ family flora_id flora_name taxon taxon_id link path """.split()
 
     def format_row(self, row):
         csv_row = {
